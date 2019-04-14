b80b39f8d88d1cf33883ccd26b5d435b0bea27be
vl5555 <v.a.pavlychenko@gmail.com>
2019-04-14 15:18:27 +0300

    2nd text

--- Rozetka/MainPage.cs
+++ Rozetka/MainPage.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using SeleniumExtras.PageObjects;
 using System.Text;
 
 namespace Rozetka
@@ -9,15 +10,15 @@ namespace Rozetka
     {
         IWebDriver driver;
 
-        private readonly string laptopsAndComputers = "//ul[@class='menu-categories menu-categories_type_main']/li[1]/a";
-        private readonly string asus = "//a[@href='https://rozetka.com.ua/notebooks/asus/c80004/v004/']";
-        private readonly string shopingCart = "//a[@href='https://rozetka.com.ua/cart/']";
-        private readonly string shopingCartPopUp = "//div[@class='header-actions__dummy-content header-actions__dummy-content_type_cart']";
+        private const string laptopsAndComputers = "//ul[@class='menu-categories menu-categories_type_main']/li[1]/a";
+        private const string asus = "//a[@href='https://rozetka.com.ua/notebooks/asus/c80004/v004/']";
+        private const string shopingCart = "//a[@href='https://rozetka.com.ua/cart/']";
+        private const string shopingCartPopUp = "//div[@class='header-actions__dummy-content header-actions__dummy-content_type_cart']";
 
         public IWebElement LaptopsAndComputers
         {
             get
-            {
+            {                
                 return driver.FindElement(By.XPath(laptopsAndComputers));
             }
         }
--- Rozetka/UnitTest1.cs
+++ Rozetka/UnitTest1.cs
@@ -19,15 +19,25 @@ namespace Rozetka
             driver.Navigate().GoToUrl("https://rozetka.com.ua/");
         }
 
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver.Quit();
+        }
+
         [Test]
         public void Test1()
         {
-            string expectedResult = $"https://rozetka.com.ua/notebooks/asus/c80004/v004/";
+            string expectedResult = $"https://rozetka.com.ua/notebooks/c80004/filter/producer=asus/";
 
-            MainPage element = new MainPage();
+            //MainPage element = new MainPage();
+
+            IWebElement laptopsAndComputers = driver.FindElement(By.XPath("//ul[@class='menu-categories menu-categories_type_main']/li[1]/a"));
+            IWebElement asus = driver.FindElement(By.XPath("//a[@href='https://rozetka.com.ua/notebooks/asus/c80004/v004/']"));
 
             Actions actions = new Actions(driver);
-            actions.MoveToElement(element.LaptopsAndComputers).MoveToElement(element.Asus).Click().Perform();
+            actions.MoveToElement(laptopsAndComputers).MoveToElement(asus).Click().Perform();
+            //actions.MoveToElement(element.LaptopsAndComputers).MoveToElement(element.Asus).Click().Perform();
 
             Assert.AreEqual(driver.Url , expectedResult,"Url of open page does not match to expected result");
         }
@@ -35,10 +45,22 @@ namespace Rozetka
         [Test]
         public void Test2()
         {
-            MainPage element = new MainPage();
+            string expectedElementText = "Ваша корзина пуста\r\nДобавляйте понравившиеся товары в корзину";
+            //MainPage element = new MainPage();
+
+            IWebElement shopingCart = driver.FindElement(By.XPath("//a[@href='https://rozetka.com.ua/cart/']"));
+            IWebElement shopingCartPopUp = driver.FindElement(By.XPath("//div[@class='header-actions__dummy-content header-actions__dummy-content_type_cart']"));
             
             Actions actions = new Actions(driver);
-            actions.MoveToElement(element.ShopingCart).MoveToElement(element.ShopingCartPupUp). Perform();
+            actions.MoveToElement(shopingCart).MoveToElement(shopingCartPopUp).Perform();
+            //actions.MoveToElement(element.ShopingCart).MoveToElement(element.ShopingCartPupUp).Perform();
+
+            //IWebElement elem = element.ShopingCartPupUp;
+            string text = shopingCartPopUp.Text;
+            Console.WriteLine(text);
+
+            Assert.AreEqual(text, expectedElementText, "Text doesn't match expected text");
+        
           
         }
     }
